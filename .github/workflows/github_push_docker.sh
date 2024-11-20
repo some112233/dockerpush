8652eaef8fa374157e44076d6aa9b6a77b6ba201
some112233 <142727467+some112233@users.noreply.github.com>
2024-11-20 15:30:30 +0800

    Update github_push_docker.sh

--- .github/workflows/github_push_docker.sh
+++ .github/workflows/github_push_docker.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# 设置目标仓库为 Docker Hub
-pull_registry="docker.io" # Docker Hub 的域名
-push_registry="crpi-lssazn1jp8ho54qv.cn-guangzhou.personal.cr.aliyuncs.com/docker-imges-c" # 您的阿里云容器镜像服务地址
+# 定义环境变量
+ALIYUN_REGISTRY_USER="${ALIYUN_REGISTRY_USER}"
+ALIYUN_REGISTRY_PASSWORD="${ALIYUN_REGISTRY_PASSWORD}"
+ALIYUN_REGISTRY="${ALIYUN_REGISTRY}"
 
-# 定义要拉取和推送的镜像列表
-images=("gitlab/gitlab-ce:latest")
+# 登录到阿里云容器镜像服务
+echo "$ALIYUN_REGISTRY_PASSWORD" | docker login -u "$ALIYUN_REGISTRY_USER" "$ALIYUN_REGISTRY" --password-stdin
 
-# 遍历镜像列表
-for image in "${images[@]}"
-do
-    # 从 Docker Hub 拉取镜像
-    docker pull "${pull_registry}/${image}"
-    # 为镜像打标签，准备推送到阿里云仓库
-    docker tag "${pull_registry}/${image}" "${push_registry}/${image%:*}"
-    # 推送镜像到阿里云仓库
-    docker push "${ALIYUN_REGISTRY}/docker-imges-c/gitlab/gitlab-ce:latest"
-done
+# 拉取 GitLab CE 镜像
+docker pull gitlab/gitlab-ce:latest
+
+# 为镜像打标签，准备推送到阿里云仓库
+docker tag gitlab/gitlab-ce:latest "$ALIYUN_REGISTRY/docker-imges-c/gitlab-ce:latest"
+
+# 推送镜像到阿里云仓库
+docker push "$ALIYUN_REGISTRY/docker-imges-c/gitlab-ce:latest"
