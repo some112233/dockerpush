712fb32f20ef2ab8895d27a7593962e1a2168706
some112233 <142727467+some112233@users.noreply.github.com>
2024-11-20 16:15:06 +0800

    Update github_push_docker.sh

--- .github/workflows/github_push_docker.sh
+++ .github/workflows/github_push_docker.sh
@@ -4,16 +4,17 @@
 ALIYUN_REGISTRY_USER="${ALIYUN_REGISTRY_USER}"
 ALIYUN_REGISTRY_PASSWORD="${ALIYUN_REGISTRY_PASSWORD}"
 ALIYUN_REGISTRY="${ALIYUN_REGISTRY}"
+IMAGES="gitlab/gitlab-ce:latest"
 
 # 登录到阿里云容器镜像服务
 docker login -u $ALIYUN_REGISTRY_USER -p $ALIYUN_REGISTRY_PASSWORD $ALIYUN_REGISTRY
 
 # 拉取 GitLab CE 镜像
-docker pull busybox:unstable-uclibc
+docker pull $IMAGES
 
 
 # 为镜像打标签，准备推送到阿里云仓库
-docker tag busybox:unstable-uclibc "$ALIYUN_REGISTRY/docker-imges-c/busybox:unstable-uclibc"
+docker tag $IMAGES "$ALIYUN_REGISTRY/docker-imges-c/$IMAGES"
 
 # 推送镜像到阿里云仓库
-docker push "$ALIYUN_REGISTRY/docker-imges-c/busybox:unstable-uclibc"
+docker push "$ALIYUN_REGISTRY/docker-imges-c/$IMAGES"
