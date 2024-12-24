ed01858190551da47a6321be50b818f7c845f313
some112233 <142727467+some112233@users.noreply.github.com>
2024-12-24 11:30:10 +0800

    Update github_push_docker.sh

--- .github/workflows/github_push_docker.sh
+++ .github/workflows/github_push_docker.sh
@@ -4,7 +4,7 @@
 ALIYUN_REGISTRY_USER="${ALIYUN_REGISTRY_USER}"
 ALIYUN_REGISTRY_PASSWORD="${ALIYUN_REGISTRY_PASSWORD}"
 ALIYUN_REGISTRY="${ALIYUN_REGISTRY}"
-IMAGES="ayushsobti/kube-monkey:v0.5.2"
+IMAGES="goharbor/prepare:v2.11.2"
 
 # 登录到阿里云容器镜像服务
 docker login -u $ALIYUN_REGISTRY_USER -p $ALIYUN_REGISTRY_PASSWORD $ALIYUN_REGISTRY
@@ -14,7 +14,7 @@ docker pull $IMAGES
 
 
 # 为镜像打标签，准备推送到阿里云仓库
-docker tag $IMAGES "$ALIYUN_REGISTRY/docker-imges-c/kube-monkey:v0.5.2"
+docker tag $IMAGES "$ALIYUN_REGISTRY/docker-imges-c/prepare:v2.11.2"
 
 # 推送镜像到阿里云仓库
-docker push "$ALIYUN_REGISTRY/docker-imges-c/kube-monkey:v0.5.2"
+docker push "$ALIYUN_REGISTRY/docker-imges-c/prepare:v2.11.2"
