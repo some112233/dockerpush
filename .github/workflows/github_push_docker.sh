f9361cc4d0c579fab14e7828cf6bdfde573d3a3b
some112233 <142727467+some112233@users.noreply.github.com>
2024-12-26 11:26:25 +0800

    Update github_push_docker.sh

--- .github/workflows/github_push_docker.sh
+++ .github/workflows/github_push_docker.sh
@@ -4,7 +4,7 @@
 ALIYUN_REGISTRY_USER="${ALIYUN_REGISTRY_USER}"
 ALIYUN_REGISTRY_PASSWORD="${ALIYUN_REGISTRY_PASSWORD}"
 ALIYUN_REGISTRY="${ALIYUN_REGISTRY}"
-IMAGES="ubuntu:24.10"
+IMAGES="koderover.tencentcloudcr.com/koderover-public/aslan:3.2.0-RELEASE.1-HOTFIX.2"
 
 # 登录到阿里云容器镜像服务
 docker login -u $ALIYUN_REGISTRY_USER -p $ALIYUN_REGISTRY_PASSWORD $ALIYUN_REGISTRY
@@ -14,7 +14,7 @@ docker pull $IMAGES
 
 
 # 为镜像打标签，准备推送到阿里云仓库
-docker tag $IMAGES "$ALIYUN_REGISTRY/docker-imges-c/ubuntu:24.10"
+docker tag $IMAGES "$ALIYUN_REGISTRY/docker-imges-c/aslan:3.2.0"
 
 # 推送镜像到阿里云仓库
-docker push "$ALIYUN_REGISTRY/docker-imges-c/ubuntu:24.10"
+docker push "$ALIYUN_REGISTRY/docker-imges-c/aslan:3.2.0"
